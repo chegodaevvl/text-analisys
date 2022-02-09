de12aac36fe3eb7b6f5eff1d0a55ff38d83c1622
chegodaevvl <84719276+chegodaevvl@users.noreply.github.com>
2022-02-09 12:55:10 +0300

    Added file verification

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from pathlib import Path
+
+
 letters_count = dict()
 
 
 def line_processing(source_line: str):
     """
         Функция обработки строки для подсчета количества уникальных букв в ней
-    :param source_line: str - Строка для обработки
+        :param source_line: str - Строка для обработки
     """
     for char in source_line:
         if char.isalpha():
@@ -14,11 +17,21 @@ def line_processing(source_line: str):
                 letters_count[char] = 1
 
 
-with open('text.txt', 'r', encoding='utf8') as source_file:
-    for file_line in source_file:
-        line_processing(file_line.lower())
-total_letters = sum(letters_count.values())
-for key in sorted(letters_count.keys()):
-    letter_frequency = round(letters_count[key] / total_letters, 4) * 100
-    print('{} - {}%'.format(key, letter_frequency))
+file_name = input('Введите абсолютный путь к файлу: ')
+if Path(file_name).exists() and Path(file_name).is_file():
+    with open(file_name, 'r', encoding='utf8') as source_file:
+        for file_line in source_file:
+            line_processing(file_line.lower())
+    total_letters = sum(letters_count.values())
+    if letters_count:
+        for key in sorted(letters_count.keys()):
+            letter_frequency = round(letters_count[key] / total_letters, 4) * 100
+            print('{} - {}%'.format(key, letter_frequency))
+    else:
+        print('Данных нет!')
+else:
+    if Path(file_name).exists():
+        print('Это не файл!')
+    else:
+        print('Файл не найден!')
 print('Работа программы завершена!')
